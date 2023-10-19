9fd6f1ebd560ab9d513ab9064d120a74e6e54cd2
Kyle Koo <kylekoo837@gmail.com>
2023-10-19 11:57:38 -0400

    lab finished, starting kanbas

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,9 +1,21 @@
+import Classes from "./Classes";
+import ConditionalOutput from "./ConditionalOutput";
 import JavaScript from "./JavaScript";
+import PathParameters from "./PathParameters";
+import Styles from "./Styles";
+import TodoItem from "./Todo/TodoItem";
+import TodoList from "./Todo/TodoList";
 
 function Assignment3() {
   return (
     <div className="container">
       <h1>Assignment 3</h1>
+      <TodoItem/>
+      <TodoList/>
+      <ConditionalOutput/>
+      <Styles/>
+      <Classes/>
+      <PathParameters/>
       <JavaScript/>
     </div>
   );
--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,10 +1,13 @@
 import Assignment3 from "./a3";
+import Nav from "../Nav";
+import {Link} from "react-router-dom";
 
 function Labs() {
-    return(
-       <div>
-          <Assignment3 />
-       </div>
+    return (
+        <div>
+            <Nav/>
+            <Assignment3 />
+        </div>
     );
- }
- export default Labs;
+}
+export default Labs;
